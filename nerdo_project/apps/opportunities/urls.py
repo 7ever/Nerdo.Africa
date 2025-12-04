8047b2462d4beb268c597ee06756cdf87b45f895
Daniel Mwago <0neeyed.folktale@gmail.com>
2025-12-04 15:01:52 +0300

    Implement CRUD operations

--- nerdo_project/apps/opportunities/urls.py
+++ nerdo_project/apps/opportunities/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.job_market, name='job_market'),
+    path('create/', views.create_job, name='create_job'),
+    path('job/<int:pk>/', views.job_detail, name='job_detail'),
 ]
