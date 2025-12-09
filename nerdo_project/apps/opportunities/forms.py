310ee46be8d24feab45da178747fc21b797be504
Daniel Mwago <0neeyed.folktale@gmail.com>
2025-12-09 18:12:12 +0300

    Debug employer form

--- nerdo_project/apps/opportunities/forms.py
+++ nerdo_project/apps/opportunities/forms.py
@@ -4,7 +4,7 @@ from .models import Job
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
-        fields = '__all__'
+        exclude = ['author', 'is_approved']
         widgets = {
             'deadline': forms.DateInput(attrs={'type': 'date'}),
             'description': forms.Textarea(attrs={'rows': 4}),
